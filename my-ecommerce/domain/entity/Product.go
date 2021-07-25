c5917b376b82034a1c68974c8d2b15b161a1b09b
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-25 18:45:39 -0400

    feat: created apply discount function on gift

--- my-ecommerce/domain/entity/Product.go
+++ my-ecommerce/domain/entity/Product.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"github.com/elissonalvesilva/interview-hash/my-ecommerce/domain/protocols"
 	valueObjects "github.com/elissonalvesilva/interview-hash/my-ecommerce/domain/value-objects"
 )
 
@@ -8,6 +9,24 @@ type Product struct {
 	ID          valueObjects.ID `json:"id"`
 	Title       valueObjects.Title `json:"title"`
 	Description valueObjects.Description `json:"description"`
-	Amount      int    `json:"amount"`
+	Amount      float64    `json:"amount"`
 	IsGift      bool   `json:"is_gift"`
+}
+
+const ISGIFT = true
+
+func ApplyDiscount(product Product, quantity int, percentage float64) protocols.ProductAppliedDiscount {
+
+	if product.IsGift == ISGIFT {
+		return protocols.ProductAppliedDiscount{
+			ID: product.ID,
+			Quantity: quantity,
+			UnitAmount: 0,
+			TotalAmount: 0,
+			Discount: 0,
+			IsGift: product.IsGift,
+		}
+	}
+
+	return protocols.ProductAppliedDiscount{}
 }
