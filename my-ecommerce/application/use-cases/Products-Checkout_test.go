aec36fa658c6448a28c015e29e47118825a262d4
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-26 15:27:31 -0400

    test: test response if service is unavailable and set 0 to discount

--- my-ecommerce/application/use-cases/Products-Checkout_test.go
+++ my-ecommerce/application/use-cases/Products-Checkout_test.go
@@ -1,6 +1,7 @@
 package use_cases
 
 import (
+	"errors"
 	"github.com/elissonalvesilva/interview-hash/my-ecommerce/domain/protocols"
 	"github.com/elissonalvesilva/interview-hash/my-ecommerce/tests/mock"
 	"github.com/golang/mock/gomock"
@@ -35,7 +36,26 @@ func TestProductCheckoutUseCase_CheckoutProducts(t *testing.T) {
 		assert.Equal(t, protocols.CheckoutResponse{}, response)
 	})
 
-	t.Run("Should checkout response with correct values", func(t *testing.T) {
+	t.Run("Should apply discount equals to 0 if service discount returns error", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+
+		mockProductCheckoutRepository := mock.NewMockProductCheckoutRepository(ctrl)
+		mockDiscountServiceRepository := mock.NewMockDiscountServiceRepository(ctrl)
+
+		gomock.InOrder(
+			mockDiscountServiceRepository.EXPECT().GetProductDiscount(gomock.Any()).Return(0.0, errors.New("service unavailable")),
+			mockDiscountServiceRepository.EXPECT().GetProductDiscount(gomock.Any()).Return(0.25, nil),
+		)
+		mockProductCheckoutRepository.EXPECT().GetProducts(gomock.Any()).Return(mock.ProductsToApplyDiscountResponse)
+
+		sut := NewProductsCheckout(mockProductCheckoutRepository, mockDiscountServiceRepository)
+
+		response := sut.CheckoutProducts(productsCheckout)
+		assert.Equal(t, mock.CheckoutResponseWithoutDiscount, response)
+	})
+
+	t.Run("Should return a response with products applied discount and total of amount, discount and applied discount", func(t *testing.T) {
 		t.Parallel()
 		ctrl := gomock.NewController(t)
 
