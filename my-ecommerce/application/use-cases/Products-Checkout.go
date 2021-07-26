6fb1f860d1a6dde25d5e2c48e83c20c4a2fe24f9
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-25 23:45:19 -0400

    feat: added response with correct values

--- my-ecommerce/application/use-cases/Products-Checkout.go
+++ my-ecommerce/application/use-cases/Products-Checkout.go
@@ -2,6 +2,7 @@ package use_cases
 
 import (
 	useCaseProtocol "github.com/elissonalvesilva/interview-hash/my-ecommerce/application/protocols"
+	"github.com/elissonalvesilva/interview-hash/my-ecommerce/domain/entity"
 	domainProtocol "github.com/elissonalvesilva/interview-hash/my-ecommerce/domain/protocols"
 )
 
@@ -23,6 +24,23 @@ func (useCase *ProductCheckoutUseCase) CheckoutProducts(productList []domainProt
 		return domainProtocol.CheckoutResponse{}
 	}
 
+	discount := 0.15
+	var totalAmount float64
+	var totalDiscount float64
+	var productsAppliedDiscount []domainProtocol.ProductAppliedDiscount
 
-	return domainProtocol.CheckoutResponse{}
+	for _, product := range products {
+		productAppliedDiscount := entity.ApplyDiscount(product, product.Quantity, discount)
+		totalAmount += productAppliedDiscount.TotalAmount
+		totalDiscount += productAppliedDiscount.Discount
+		productsAppliedDiscount = append(productsAppliedDiscount, productAppliedDiscount)
+	}
+
+	totalAmountWithDiscount := totalAmount - totalDiscount
+	return domainProtocol.CheckoutResponse{
+		TotalAmount: totalAmount,
+		TotalAmountWithDiscount: totalAmountWithDiscount,
+		TotalDiscount: totalDiscount,
+		Products: productsAppliedDiscount,
+	}
 }
