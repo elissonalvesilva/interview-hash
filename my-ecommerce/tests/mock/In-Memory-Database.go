54a0856150c8d158362be865d261b1585ae9a0a5
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-26 19:10:57 -0400

    feat: add new params to mock

--- my-ecommerce/tests/mock/In-Memory-Database.go
+++ my-ecommerce/tests/mock/In-Memory-Database.go
@@ -1,10 +1,11 @@
 package mock
 
 import (
-	"reflect"
+	reflect "reflect"
 
-	"github.com/elissonalvesilva/interview-hash/my-ecommerce/domain/entity"
-	"github.com/golang/mock/gomock"
+	entity "github.com/elissonalvesilva/interview-hash/my-ecommerce/domain/entity"
+	protocols "github.com/elissonalvesilva/interview-hash/my-ecommerce/infrastructure/db/in-memory/protocols"
+	gomock "github.com/golang/mock/gomock"
 )
 
 // MockInMemory is a mock of InMemory interface.
@@ -43,4 +44,19 @@ func (m *MockInMemory) GetByID(id int) (entity.Product, error) {
 func (mr *MockInMemoryMockRecorder) GetByID(id interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetByID", reflect.TypeOf((*MockInMemory)(nil).GetByID), id)
+}
+
+// GetOne mocks base method.
+func (m *MockInMemory) GetOne(filter protocols.Filter) (entity.Product, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetOne", filter)
+	ret0, _ := ret[0].(entity.Product)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetOne indicates an expected call of GetOne.
+func (mr *MockInMemoryMockRecorder) GetOne(filter interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetOne", reflect.TypeOf((*MockInMemory)(nil).GetOne), filter)
 }
