867f128d14a2cfe1265106a011bfd414bf333b84
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-25 22:30:33 -0400

    feat: add product checkout return protocol

--- my-ecommerce/domain/protocols/Product-Protocols.go
+++ my-ecommerce/domain/protocols/Product-Protocols.go
@@ -1,6 +1,15 @@
 package protocols
 
+import valueObjects "github.com/elissonalvesilva/interview-hash/my-ecommerce/domain/value-objects"
+
 type ProductCheckout struct {
 	ID int
 	Quantity int
+}
+
+type ProductToApplyDiscount struct {
+	ID valueObjects.ID
+	Amount float64
+	Quantity int
+	IsGift bool
 }
