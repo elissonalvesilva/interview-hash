a78537924811d38f6b186640cd6e9d93ea6a1ed9
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-27 16:24:16 -0400

    feat: created checkout controller

---
+++ my-ecommerce/presenters/controllers/Checkout-Controller.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"encoding/json"
+	"github.com/asaskevich/govalidator"
+	useCases "github.com/elissonalvesilva/interview-hash/my-ecommerce/application/use-cases"
+	domainProtocol "github.com/elissonalvesilva/interview-hash/my-ecommerce/domain/protocols"
+	"net/http"
+)
+
+type CheckoutController struct {
+	useCase useCases.ProductCheckoutUseCase
+}
+
+func NewCheckoutController(useCase useCases.ProductCheckoutUseCase) *CheckoutController {
+	return &CheckoutController{
+		useCase: useCase,
+	}
+}
+
+func (ctrl *CheckoutController) CheckoutProductsController (w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	var productCheckoutRequest domainProtocol.ProductCheckoutRequest
+	errDecode := decoder.Decode(&productCheckoutRequest)
+
+	if errDecode != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode("Invalid request param")
+		return
+	}
+
+	_, errorValidationParam := govalidator.ValidateStruct(productCheckoutRequest)
+	if errorValidationParam != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errorValidationParam)
+		return
+	}
+
+	checkoutProduct := ctrl.useCase.CheckoutProducts(productCheckoutRequest.Products)
+
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(checkoutProduct)
+	return
+}
