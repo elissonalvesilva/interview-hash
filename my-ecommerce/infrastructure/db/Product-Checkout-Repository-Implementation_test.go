99fce3ec880ba8ff90d6eee9d5bcf2f83a50ed91
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-26 21:54:11 -0400

    test: added function to get product gift in db

--- my-ecommerce/infrastructure/db/Product-Checkout-Repository-Implementation_test.go
+++ my-ecommerce/infrastructure/db/Product-Checkout-Repository-Implementation_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"github.com/elissonalvesilva/interview-hash/my-ecommerce/domain/entity"
 	"github.com/elissonalvesilva/interview-hash/my-ecommerce/domain/protocols"
+	inMemoryDB "github.com/elissonalvesilva/interview-hash/my-ecommerce/infrastructure/db/in-memory"
 	"github.com/elissonalvesilva/interview-hash/my-ecommerce/tests/mock"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
@@ -18,9 +19,9 @@ func TestProductCheckoutRepositoryImplementation_GetProducts(t *testing.T) {
 		productListToApplyDiscount := mock.ProductsToCheckout
 
 		gomock.InOrder(
-			mockInMemoryDatabase.EXPECT().GetByID(1).Return(entity.Product{}, ErrNotFoundItemInDB),
-			mockInMemoryDatabase.EXPECT().GetByID(2).Return(entity.Product{}, ErrNotFoundItemInDB),
-			mockInMemoryDatabase.EXPECT().GetByID(3).Return(entity.Product{}, ErrNotFoundItemInDB),
+			mockInMemoryDatabase.EXPECT().GetByID(1).Return(entity.Product{}, inMemoryDB.ErrNotFoundItemInDB),
+			mockInMemoryDatabase.EXPECT().GetByID(2).Return(entity.Product{}, inMemoryDB.ErrNotFoundItemInDB),
+			mockInMemoryDatabase.EXPECT().GetByID(3).Return(entity.Product{}, inMemoryDB.ErrNotFoundItemInDB),
 		)
 
 		sut := NewProductCheckoutRepositoryImplementation(mockInMemoryDatabase)
@@ -46,5 +47,42 @@ func TestProductCheckoutRepositoryImplementation_GetProducts(t *testing.T) {
 
 		response := sut.GetProducts(productListToApplyDiscount)
 		assert.Equal(t, mock.AllProductsToApplyDiscount, response)
+	})
+}
+
+func TestProductCheckoutRepositoryImplementation_GetProductToGift(t *testing.T) {
+	t.Run("Should return GetOne throws or not found filter condition", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := gomock.NewController(t)
+		mockInMemoryDatabase := mock.NewMockInMemory(ctrl)
+
+		mockInMemoryDatabase.EXPECT().GetOne(gomock.Any()).Return(entity.Product{}, inMemoryDB.ErrNotFoundItemInDBByCondition)
+
+		sut := NewProductCheckoutRepositoryImplementation(mockInMemoryDatabase)
+
+		response := sut.GetProductToGift()
+		assert.Equal(t, protocols.ProductAppliedDiscount{}, response)
+	})
+
+	t.Run("Should return product gift to added", func(t *testing.T) {
+		t.Parallel()
+
+		ctrl := gomock.NewController(t)
+		mockInMemoryDatabase := mock.NewMockInMemory(ctrl)
+
+		mockInMemoryDatabase.EXPECT().GetOne(gomock.Any()).Return(mock.Product3, nil)
+
+		sut := NewProductCheckoutRepositoryImplementation(mockInMemoryDatabase)
+
+		response := sut.GetProductToGift()
+		assert.Equal(t, protocols.ProductAppliedDiscount{
+			ID: mock.Product3.ID,
+			Quantity: 1,
+			TotalAmount: 0,
+			UnitAmount: 0,
+			Discount: 0,
+			IsGift: mock.Product3.IsGift,
+		}, response)
 	})
 }
