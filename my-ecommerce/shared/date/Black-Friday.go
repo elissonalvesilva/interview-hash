3640436c971e46bb37bc3cd069daf852c7d199ba
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-27 18:19:44 -0400

    refactor: change validation black friday date

--- my-ecommerce/shared/date/Black-Friday.go
+++ my-ecommerce/shared/date/Black-Friday.go
@@ -7,8 +7,7 @@ import (
 func IsBlackFriday(actualDate time.Time, blackFridayDate time.Time) bool {
 	isBlackFriday := false
 
-	if actualDate.Year() == blackFridayDate.Year() &&
-		actualDate.Month() == blackFridayDate.Month() &&
+	if 	actualDate.Month() == blackFridayDate.Month() &&
 		actualDate.Day() == blackFridayDate.Day(){
 		isBlackFriday = true
 		return isBlackFriday
