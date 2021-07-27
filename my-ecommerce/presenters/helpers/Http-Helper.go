fc7f8cd304348cf04002b6e1469e8ff0d7ca603c
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-27 16:58:24 -0400

    refactor: remove returns

--- my-ecommerce/presenters/helpers/Http-Helper.go
+++ my-ecommerce/presenters/helpers/Http-Helper.go
@@ -8,17 +8,14 @@ import (
 func Ok(w http.ResponseWriter, body interface{}) {
 	w.WriteHeader(200)
 	json.NewEncoder(w).Encode(body)
-	return
 }
 
 func BadRequest(w http.ResponseWriter, err error) {
 	w.WriteHeader(http.StatusBadRequest)
 	json.NewEncoder(w).Encode(err)
-	return
 }
 
 func NotFound(w http.ResponseWriter, err error) {
 	w.WriteHeader(http.StatusNotFound)
 	json.NewEncoder(w).Encode(err)
-	return
 }
