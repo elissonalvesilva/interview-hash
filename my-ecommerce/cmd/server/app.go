c40c8cb9bfc9a449d4b4a68b1df45af2db59a7f6
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-27 18:21:25 -0400

    feat: created app and added function to run

---
+++ my-ecommerce/cmd/server/app.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"fmt"
+	"github.com/elissonalvesilva/interview-hash/my-ecommerce/cmd/server/factories/controllers"
+	inMemoryDB "github.com/elissonalvesilva/interview-hash/my-ecommerce/infrastructure/db/in-memory"
+	"github.com/elissonalvesilva/interview-hash/my-ecommerce/shared/file"
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+type App struct {
+	httpServer *http.Server
+}
+
+func (a *App) Run(application string) {
+	fmt.Println(application+" Is running in port", a.httpServer.Addr)
+	err := a.httpServer.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NewApp(port int) *App {
+	db := file.ReadDBFile(os.Getenv("DB_IN_MEMORY_PATHNAME"))
+	var blackFridayDate time.Time
+
+	database := inMemoryDB.NewInMemoryDatabase(db)
+
+	blackFridayDay, _ := strconv.Atoi(os.Getenv("BLACK_FRIDAY_DAY"))
+	blackFridayMonth, _ := strconv.Atoi(os.Getenv("BLACK_FRIDAY_MONTH"))
+	blackFridayDate = time.Date(time.Now().Year(), time.Month(blackFridayMonth), blackFridayDay, 00, 00, 00, 00, time.UTC)
+
+	router := mux.NewRouter()
+
+	controller := controllers.MakeCheckoutController(database, blackFridayDate)
+
+	router.HandleFunc("/checkout", controller.CheckoutProductsController).Methods("POST")
+
+	return &App{
+		httpServer: &http.Server{
+			Addr:    fmt.Sprintf(":%v", port),
+			Handler: router,
+		},
+	}
+}
