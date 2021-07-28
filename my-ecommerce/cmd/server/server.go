40872bf416efecf91f8840a9ecb643912f7e1ae2
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-27 22:54:11 -0400

    feat: added timezone in project

--- my-ecommerce/cmd/server/server.go
+++ my-ecommerce/cmd/server/server.go
@@ -2,12 +2,21 @@ package server
 
 import (
 	"github.com/elissonalvesilva/interview-hash/my-ecommerce/infrastructure/env/dotenv"
+	"log"
 	"os"
 	"strconv"
+	"time"
 )
 
 func Run() {
 	dotenv.InitEnvironment()
+
+	location, errToGetTimezone := time.LoadLocation(os.Getenv("TIMEZONE"))
+	if errToGetTimezone != nil {
+		log.Fatal(errToGetTimezone)
+		return
+	}
+	time.Local = location
 	port, _ := strconv.Atoi(os.Getenv("APP_PORT"))
 	name := os.Getenv("APP_NAME")
 
