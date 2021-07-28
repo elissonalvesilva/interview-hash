cd23539a0f6667f6d6b072fcc9e4ec2842dc812b
Elisson Alves Silva <elissonalvessilva@192.168.1.9>
2021-07-28 12:03:40 -0400

    test: added function to parse to cents

--- tests/unit/shared/currency/Currency_test.go
+++ tests/unit/shared/currency/Currency_test.go
@@ -17,3 +17,32 @@ func TestTruncateNaive(t *testing.T) {
 		assert.Equal(t, 152.00, number)
 	})
 }
+
+func TestParseToCents(t *testing.T) {
+	t.Run("Should return 0 if value param is equals to 0", func(t *testing.T) {
+		t.Parallel()
+
+		valueParam := 0.0
+
+		response := currency.ParseToCents(valueParam)
+		assert.Equal(t, int64(0), response)
+	})
+
+	t.Run("Should return a response in cents if has number after dot", func(t *testing.T) {
+		t.Parallel()
+
+		valueParam := 548.15
+
+		response := currency.ParseToCents(valueParam)
+		assert.Equal(t, int64(54815), response)
+	})
+
+	t.Run("Should return a response in cents if no has number after dot", func(t *testing.T) {
+		t.Parallel()
+
+		valueParam := 548.00
+
+		response := currency.ParseToCents(valueParam)
+		assert.Equal(t, int64(54800), response)
+	})
+}
